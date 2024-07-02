1faf44243f14d8e196e3d337490140f1eb060e6a
Yeyson Cañon <yeyson_53@hotmail.com>
2024-07-01 21:09:59 -0500

    Update polyfill_tester.py
    Se adiciona la validación de los siguientes dominios:
    
    bootcdn.net
    bootcss.com
    staticfile.net
    staticfile.org
    unionadjs.com
    xhsbpza.com
    union.macoms.la
    newcrbpc.com

--- polyfill_tester.py
+++ polyfill_tester.py
@@ -16,17 +16,34 @@ output_file = 'results.csv'
 session = HTMLSession()
 session.verify = False
 
+# Lista de dominios a verificar
+domains_to_check = [
+    'polyfill.io',
+    'bootcdn.net',
+    'bootcss.com',
+    'staticfile.net',
+    'staticfile.org',
+    'unionadjs.com',
+    'xhsbpza.com',
+    'union.macoms.la',
+    'newcrbpc.com'
+]
+
 def is_vulnerable(url):
+    found_domains = []
+    found_lines = []
     try:
         response: HTMLResponse = session.get(url)
         response.html.render(timeout=20)
         html_lines = response.html.html.splitlines()
         for line in html_lines:
-            if 'polyfill.io' in line:
-                return True, line.strip()
+            for domain in domains_to_check:
+                if domain in line and domain not in found_domains:
+                    found_domains.append(domain)
+                    found_lines.append(line.strip())
     except Exception as e:
         print(f"{Fore.RED}Error al acceder a {url}: {e}{Style.RESET_ALL}")
-    return False, None
+    return found_domains, found_lines
 
 with open(args.input_file, 'r') as file:
     urls = [line.strip() for line in file]
@@ -34,17 +51,20 @@ with open(args.input_file, 'r') as file:
 results = []
 for url in urls:
     print(f"{Fore.YELLOW}Verificando {url}...{Style.RESET_ALL}")
-    vulnerable, line_content = is_vulnerable(url)
-    if vulnerable:
-        print(f"{Fore.GREEN}URL: {url} es potencialmente vulnerable. Contenido: {line_content}{Style.RESET_ALL}")
-        results.append([url, 'Sí'])
+    found_domains, found_lines = is_vulnerable(url)
+    if found_domains:
+        domains_str = ', '.join(found_domains)
+        lines_str = '\n'.join(found_lines)
+        print(f"{Fore.GREEN}URL: {url} es potencialmente vulnerable. Dominios encontrados: {domains_str}{Style.RESET_ALL}")
+        print(f"{Fore.GREEN}Líneas de código vulnerables:\n{lines_str}{Style.RESET_ALL}")
+        results.append([url, 'Sí', domains_str])
     else:
         print(f"{Fore.CYAN}URL: {url} no es potencialmente vulnerable.{Style.RESET_ALL}")
-        results.append([url, 'No'])
+        results.append([url, 'No', ''])
 
 with open(output_file, 'w', newline='') as csvfile:
     csvwriter = csv.writer(csvfile)
-    csvwriter.writerow(['URL', 'Potencialmente Vulnerable'])
+    csvwriter.writerow(['URL', 'Potencialmente Vulnerable', 'Dominios Encontrados'])
     csvwriter.writerows(results)
 
 print(f"{Fore.MAGENTA}Resultados guardados en {output_file}{Style.RESET_ALL}")
